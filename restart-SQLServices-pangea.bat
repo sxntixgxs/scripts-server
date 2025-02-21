ef173115f9430303c578b28cc3bd5fc795bec9d2
sxntixgxs <sandovalbgasantiago@gmail.com>
2025-02-21 11:05:19 -0500

    change order queries

--- restart-SQLServices-pangea.bat
+++ restart-SQLServices-pangea.bat
@@ -1,16 +1,18 @@
 @echo off
 echo Restarting services ...
+net stop "SQLServerReportingServices"
+net stop "ReportServer"
+net stop "MSSQLFDLauncher"
+net stop "SQLWriter"
+net stop "MSSQLSERVER"
 net stop "SQLBrowser"
 net start "SQLBrowser"
-net stop "MSSQLSERVER"
-net start "MSSQLSERVER"
-net stop "SQLServerReportingServices"
-net start "SQLServerReportingServices"
-net stop "ReportServer"
-net start "ReportServer"
-net stop "SQLWriter"
+net start "MSSQLServer"
 net start "SQLWriter"
-net stop "MSSQLFDLauncher"
 net start "MSSQLFDLauncher"
+net start "SQLServerReportingServices"
+net start "ReportServer"
 echo Services restarted successfully
-pause
+:: timeout /t 5 /nobreak >nul
+:: echo Restarting sistem ...
+:: shutdown /r /f /t 0
